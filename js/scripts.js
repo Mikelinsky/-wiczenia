e4529ace95eea181c4f17c2e903509f821bf7361
Michał Kupiecki <michal481216@gmail.com>
2018-02-05 13:43:32 +0100

    edit ramdom prophecy

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var answerList = [
     'Przemyśl to jeszcze, a potem zrób',
     'Nie ma próbowania, jest robienie',
     'To kiepski pomysł',
-    'To jeszcze nie czas, zrób to w przyszłości'
+    'To jeszcze nie czas, zrób to w przyszłości',
     'Pomelo je się w kuchni'];
 
 var value = Math.floor(Math.random() * 6);
