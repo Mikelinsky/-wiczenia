4fb4692bc57fec7a4bd67378b27084767d053e88
Michał Kupiecki <michal481216@gmail.com>
2018-02-05 13:41:28 +0100

    add ramdom prophecy

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
-var question = prompt('Do you like strawberries?');				
-										
-switch (question.toLowerCase()) {						
-  case 'yes':									
-    alert('Great, me too!');							
-    break;									
-  case 'no':									
-    alert('really?');								
-    break;									
-  default:									
-    alert("You did not give a clear answer, so you're probably still wondering.");	
-}	
+prompt('Spytaj wyrocznię o zdanie.')
+var answerList = [
+    'Koniecznie to zrób!',
+    'Przemyśl to jeszcze, a potem zrób',
+    'Nie ma próbowania, jest robienie',
+    'To kiepski pomysł',
+    'To jeszcze nie czas, zrób to w przyszłości'
+    'Pomelo je się w kuchni'];
+
+var value = Math.floor(Math.random() * 6);
+
+alert('Twoja odpowiedź brzmi: ' + answerList[value]);
+
